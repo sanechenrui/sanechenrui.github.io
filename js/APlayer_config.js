13e2a6160e4a70b7bc15dad4c291d636e52a4fc8
sanechenrui <770624174@qq.com>
2020-06-12 15:17:35 +0800

    Site updated: 2020-06-12 15:17:35

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -12,18 +12,12 @@ const ap = new APlayer({
     listFolded: false,
 
     audio: [{
-            name: '一直很安静',
-            artist: '阿桑',
-            lrc: '/music/lrc/一直很安静 - 阿桑.lrc',
-            cover: 'https://p1.music.126.net/SpovasHBud2A1qXXADXsBg==/109951163167455610.jpg?param=300x300',
-            url: 'http://q0fzyzixq.bkt.clouddn.com/audio/mp3/一直很安静 - 阿桑.mp3'
-                  },
-        {
-            name: '亲爱的旅人啊（Cover：木村弓）',
-            artist: '周深',
-            lrc: '/music/lrc/亲爱的旅人啊（Cover：木村弓）-周深.lrc',
-            cover: 'https://p1.music.126.net/1YrCPOBV314i-mTtlDg8mQ==/109951164148664637.jpg?param=300x300',
-            url: 'http://q0fzyzixq.bkt.clouddn.com/audio/mp3/亲爱的旅人啊（Cover：木村弓） - 周深.mp3'
+            name: '与我无关',
+            artist: '阿冗',
+            lrc: '',
+            cover: 'http://p1.music.126.net/x-jReyGkM5OTKUEtTqXGoA==/109951164597332931.jpg?param=300x300',
+            url: 'http://music.163.com/song/media/outer/url?id=1413585838.mp3'
                   }
+
         ]
 });
